d6ea3adda8b2a3848f4573311851ba3b6cf627d6
Kevin Heritage <kheritage222@gmail.com>
2020-04-23 20:23:53 +0200

    Update genre extractor with more robust tests

--- Jellyfin.Plugin.Extractor.Tests/Services/GenreExtractorServiceTest.cs
+++ Jellyfin.Plugin.Extractor.Tests/Services/GenreExtractorServiceTest.cs
@@ -2,7 +2,7 @@ using Xunit;
 using Jellyfin.Plugin.Extractor.Services;
 using System.Collections.Generic;
 
-namespace Jellyfin.Plugin.Extractor.Tests
+namespace Jellyfin.Plugin.Extractor.Tests.Services
 {
   public class GenreExtractorServiceTests
   {
@@ -38,43 +38,68 @@ namespace Jellyfin.Plugin.Extractor.Tests
       Assert.Equal(new List<string>() { "genre" }, genres);
     }
 
-    [Fact]
-    public void extractGenres_ShouldReturnGenres()
+    [Theory]
+    [InlineData("Moviename - actor - (genre, genre)")]
+    [InlineData("(genre, genre) Movie - actor")]
+    [InlineData("Movie (genre, genre) - actor")]
+    public void extractGenres_ShouldReturnGenres(string movie)
     {
-      //Given
-      var movie = "Moviename - actor - (genre, genre2)";
-
       //When
       var genres = genreExtractorService.extractGenres(movie);
 
       //Then
-      Assert.Equal(new List<string>() { "genre", "genre2" }, genres);
+      Assert.Equal(new List<string>() { "genre", "genre" }, genres);
     }
 
     [Fact]
     public void removeGenresFromMovieName_ShouldReturnMovieWithNoAlterations()
     {
-    //Given
-    var movie = "MovieName - actor";
-    
-    //When
-    var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
+      //Given
+      var movie = "MovieName - actor";
 
-    //Then
-    Assert.Equal(movie, updatedMovie);
+      //When
+      var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
+
+      //Then
+      Assert.Equal(movie, updatedMovie);
     }
 
     [Fact]
-    public void removeGenresFromMovieName_ShouldReturnAMovieWithoutGenres()
+    public void removeGenresFromMovieName_ShouldReturnAMovieWithoutGenres_WhenGenresAtEnd()
     {
-    //Given
-    var movie = "MovieName - actor (genre1, genre3)";
-    
-    //When
-    var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
-    
-    //Then
-    Assert.Equal("MovieName - actor", updatedMovie);
+      //Given
+      var movie = "MovieName - actor (genre1, genre3)";
+
+      //When
+      var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
+
+      //Then
+      Assert.Equal("MovieName - actor", updatedMovie);
+    }
+
+    [Fact]
+    public void removeGenresFromMovieName_ShouldReturnAMovieWithoutGenres_WhenGenresAtMiddle()
+    {
+      //Given
+      var movie = "MovieName (genre1, genre3) actor";
+
+      //When
+      var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
+
+      //Then
+      Assert.Equal("MovieName actor", updatedMovie);
+    }
+
+    [Fact]
+    public void removeGenresFromMovieName_ShouldReturnAMovieWithoutGenres_WhenGenresAtStart()    {
+      //Given
+      var movie = "(genre1, genre3) MovieName - actor";
+
+      //When
+      var updatedMovie = genreExtractorService.removeGenresFromMovie(movie);
+
+      //Then
+      Assert.Equal("MovieName - actor", updatedMovie);
     }
   }
 }
--- Jellyfin.Plugin.Extractor/Services/GenreExtractorService.cs
+++ Jellyfin.Plugin.Extractor/Services/GenreExtractorService.cs
@@ -5,14 +5,31 @@ namespace Jellyfin.Plugin.Extractor.Services
 {
   public class GenreExtractorService : IGenreExtractorService
   {
+    private char genreOpenChar;
+    private char genreClosingChar;
+    private char genreDelimiter;
+
+    public GenreExtractorService()
+    {
+      this.genreOpenChar = '(';
+      this.genreClosingChar = ')';
+      this.genreDelimiter = ',';
+    }
+    public GenreExtractorService(char genreOpenChar, char genreClosingChar, char genreDelimiter)
+    {
+      this.genreOpenChar = genreOpenChar;
+      this.genreClosingChar = genreClosingChar;
+      this.genreDelimiter = genreDelimiter;
+    }
+
     public IEnumerable<string> extractGenres(string movieName)
     {
-      int openIndex = movieName.IndexOf("(");
-      int closedIndex = movieName.IndexOf(")");
-      if (openIndex > 0 && closedIndex > 0)
+      int openIndex = movieName.IndexOf(genreOpenChar);
+      int closedIndex = movieName.IndexOf(genreClosingChar);
+      if (openIndex >= 0 && closedIndex > 0)
       {
         return movieName
-        .Substring(openIndex + 1, closedIndex - openIndex - 1).Split(',').Select(genre => genre.Trim());
+        .Substring(openIndex + 1, closedIndex - openIndex - 1).Split(genreDelimiter).Select(genre => genre.Trim());
       }
 
       return new List<string>();
@@ -20,10 +37,13 @@ namespace Jellyfin.Plugin.Extractor.Services
 
     public string removeGenresFromMovie(string movieName)
     {
-      var openIndex = movieName.LastIndexOf(" (");
-      if (openIndex > 0)
+      var openIndex = movieName.IndexOf(genreOpenChar);
+      var closedIndex = movieName.IndexOf(genreClosingChar);
+      if (openIndex >= 0 && closedIndex > 0)
       {
-        movieName = movieName.Substring(0, openIndex);
+        var before = movieName.Substring(0, openIndex).Trim();
+        var after = movieName.Substring(closedIndex + 1).Trim();
+        movieName = $"{before} {after}".Trim();
       }
       return movieName;
     }
