97acbf13a4309437814a7a3e7df12090953910e5
Kevin Heritage <kheritage222@gmail.com>
2020-04-23 12:45:03 +0200

    Remove unnecessary dependencies

--- Jellyfin.Plugin.Extractor/Plugin.cs
+++ Jellyfin.Plugin.Extractor/Plugin.cs
@@ -3,13 +3,8 @@ using System.Collections.Generic;
 using Jellyfin.Plugin.Extractor.Configuration;
 using MediaBrowser.Common.Configuration;
 using MediaBrowser.Common.Plugins;
-using MediaBrowser.Controller.Entities;
-using MediaBrowser.Controller.Library;
 using MediaBrowser.Model.Plugins;
 using MediaBrowser.Model.Serialization;
-using Microsoft.Extensions.Logging;
-using System.Threading;
-using System.Linq;
 
 namespace Jellyfin.Plugin.Extractor
 {
