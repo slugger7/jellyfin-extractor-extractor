92c6d18d92e8aa08d12568f1b231834e59e4556f
Kevin Heritage <kheritage222@gmail.com>
2020-04-23 17:05:43 +0200

    Updated version number

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+jellyfinConfig=~/.config/jellyfin
 dotnet build Jellyfin.Plugin.Extractor.sln && \
-docker stop jellyfin-dev && \
-rm ~/Documents/jellyfinconfig/data/plugins/Extractor/Jellyfin.Plugin.Extractor.dll && \
-cp Jellyfin.Plugin.Extractor/bin/Debug/netstandard2.1/Jellyfin.Plugin.Extractor.dll ~/Documents/jellyfinconfig/data/plugins/Extractor/Jellyfin.Plugin.Extractor.dll &&\
-docker start jellyfin-dev && \
-docker logs -f jellyfin-dev;
+docker stop jellyfin-test && \
+rm $jellyfinConfig/data/plugins/Extractor/Jellyfin.Plugin.Extractor.dll && \
+cp Jellyfin.Plugin.Extractor/bin/Debug/netstandard2.1/Jellyfin.Plugin.Extractor.dll $jellyfinConfig/data/plugins/Extractor/Jellyfin.Plugin.Extractor.dll &&\
+docker start jellyfin-test && \
+docker logs -f jellyfin-test;
